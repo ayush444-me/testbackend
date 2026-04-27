07baa1bb6f47757867858eefb738641588f9b8a4
Ayush Chauhan <ayushchauhan5458@gmail.com>
2026-04-27 10:07:24 +0530

    testing

---
+++ Test_01.js
@@ -0,0 +1,18 @@
+// console.log("Testing");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+    res.send('hello world')
+
+    
+})
+app.get('/Home',  (req, res) => {
+    res.send('Welcome Home Page')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`${port}`)
+
+})
